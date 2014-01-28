dab7b5a48fce5b840fcb1d6cd9cf812366b41437
fmartelli <fabio.martelli@gmail.com>
2014-01-28 15:54:25 +0100

    Fixes #154

---
+++ plugin/src/main/java/com/msopentech/odatajclient/plugin/V4MetadataMojo.java
@@ -0,0 +1,296 @@
+/**
+ * Copyright © Microsoft Open Technologies, Inc.
+ *
+ * All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+ * ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A
+ * PARTICULAR PURPOSE, MERCHANTABILITY OR NON-INFRINGEMENT.
+ *
+ * See the Apache License, Version 2.0 for the specific language
+ * governing permissions and limitations under the License.
+ */
+package com.msopentech.odatajclient.plugin;
+
+import com.msopentech.odatajclient.engine.client.ODataClientFactory;
+import com.msopentech.odatajclient.engine.communication.request.retrieve.ODataV3MetadataRequest;
+import com.msopentech.odatajclient.engine.communication.response.ODataRetrieveResponse;
+import com.msopentech.odatajclient.engine.data.metadata.EdmV3Metadata;
+import com.msopentech.odatajclient.engine.data.metadata.edm.v3.ComplexType;
+import com.msopentech.odatajclient.engine.data.metadata.edm.v3.EntityContainer;
+import com.msopentech.odatajclient.engine.data.metadata.edm.v3.EntitySet;
+import com.msopentech.odatajclient.engine.data.metadata.edm.v3.EntityType;
+import com.msopentech.odatajclient.engine.data.metadata.edm.v3.Schema;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
+
+/**
+ * POJOs generator.
+ */
+@Mojo(name = "pojos", defaultPhase = LifecyclePhase.PROCESS_SOURCES)
+public class MetadataMojo extends AbstractMojo {
+
+    /**
+     * Generated files base root.
+     */
+    @Parameter(property = "outputDirectory", required = true)
+    private String outputDirectory;
+
+    /**
+     * OData service root URL.
+     */
+    @Parameter(property = "serviceRootURL", required = true)
+    private String serviceRootURL;
+
+    /**
+     * Base package.
+     */
+    @Parameter(property = "basePackage", required = true)
+    private String basePackage;
+
+    private Utility utility = null;
+
+    private final Set<String> namespaces = new HashSet<String>();
+
+    private static String TOOL_DIR = "ojc-plugin";
+
+    @Override
+    public void execute() throws MojoExecutionException {
+        if (new File(outputDirectory + File.separator + TOOL_DIR).exists()) {
+            getLog().info("Nothing to do because " + TOOL_DIR + " directory already exists. Clean to update.");
+            return;
+        }
+
+        try {
+            Velocity.addProperty(Velocity.RESOURCE_LOADER, "class");
+            Velocity.addProperty("class.resource.loader.class", ClasspathResourceLoader.class.getName());
+
+            final ODataV3MetadataRequest req = ODataClientFactory.getV3().
+                    getRetrieveRequestFactory().getMetadataRequest(serviceRootURL);
+
+            final ODataRetrieveResponse<EdmV3Metadata> res = req.execute();
+            final EdmV3Metadata metadata = res.getBody();
+
+            if (metadata == null) {
+                throw new IllegalStateException("Metadata not found");
+            }
+
+            for (Schema schema : metadata.getSchemas()) {
+                namespaces.add(schema.getNamespace().toLowerCase());
+            }
+
+            final Set<String> complexTypeNames = new HashSet<String>();
+            final File services = mkdir("META-INF/services");
+
+            for (Schema schema : metadata.getSchemas()) {
+                utility = new Utility(metadata, schema, basePackage);
+
+                // write package-info for the base package
+                final String schemaPath = utility.getNamespace().toLowerCase().replace('.', File.separatorChar);
+                final File base = mkPkgDir(schemaPath);
+                final String pkg = basePackage + "." + utility.getNamespace().toLowerCase();
+                parseObj(base, pkg, "package-info", "package-info.java");
+
+                // write package-info for types package
+                final File typesBaseDir = mkPkgDir(schemaPath + "/types");
+                final String typesPkg = pkg + ".types";
+                parseObj(typesBaseDir, typesPkg, "package-info", "package-info.java");
+
+                final Map<String, Object> objs = new HashMap<String, Object>();
+
+                // write types into types package
+                for (ComplexType complex : schema.getComplexTypes()) {
+                    final String className = utility.capitalize(complex.getName());
+                    complexTypeNames.add(typesPkg + "." + className);
+                    objs.clear();
+                    objs.put("complexType", complex);
+                    parseObj(typesBaseDir, typesPkg, "complexType", className + ".java", objs);
+                }
+
+                for (EntityType entity : schema.getEntityTypes()) {
+                    objs.clear();
+                    objs.put("entityType", entity);
+
+                    final Map<String, String> keys;
+
+                    EntityType baseType = null;
+                    if (entity.getBaseType() == null) {
+                        keys = utility.getEntityKeyType(entity);
+                    } else {
+                        baseType = schema.getEntityType(utility.getNameFromNS(entity.getBaseType()));
+                        objs.put("baseType", utility.getJavaType(entity.getBaseType()));
+                        while (baseType.getBaseType() != null) {
+                            baseType = schema.getEntityType(utility.getNameFromNS(baseType.getBaseType()));
+                        }
+                        keys = utility.getEntityKeyType(baseType);
+                    }
+
+                    if (keys.size() > 1) {
+                        // create compound key class
+                        final String keyClassName = utility.capitalize(baseType == null
+                                ? entity.getName()
+                                : baseType.getName()) + "Key";
+                        objs.put("keyRef", keyClassName);
+
+                        if (entity.getBaseType() == null) {
+                            objs.put("keys", keys);
+                            parseObj(typesBaseDir, typesPkg, "entityTypeKey", keyClassName + ".java", objs);
+                        }
+                    }
+
+                    parseObj(typesBaseDir, typesPkg, "entityType",
+                            utility.capitalize(entity.getName()) + ".java", objs);
+                    parseObj(typesBaseDir, typesPkg, "entityCollection",
+                            utility.capitalize(entity.getName()) + "Collection.java", objs);
+                }
+
+                // write container and top entity sets into the base package
+                for (EntityContainer container : schema.getEntityContainers()) {
+                    objs.clear();
+                    objs.put("container", container);
+                    parseObj(base, pkg, "container",
+                            utility.capitalize(container.getName()) + ".java", objs);
+
+                    for (EntitySet entitySet : container.getEntitySets()) {
+                        objs.clear();
+                        objs.put("entitySet", entitySet);
+                        parseObj(base, pkg, "entitySet",
+                                utility.capitalize(entitySet.getName()) + ".java", objs);
+                    }
+                }
+
+                parseObj(services, true, null, "services", "com.msopentech.odatajclient.proxy.api.AbstractComplexType",
+                        Collections.singletonMap("services", (Object) complexTypeNames));
+            }
+        } catch (Throwable t) {
+            final StringWriter stringWriter = new StringWriter();
+            final PrintWriter printWriter = new PrintWriter(stringWriter);
+            t.printStackTrace(printWriter);
+            getLog().error(stringWriter.toString());
+
+            throw (t instanceof MojoExecutionException)
+                    ? (MojoExecutionException) t
+                    : new MojoExecutionException("While executin mojo", t);
+        }
+    }
+
+    private File mkdir(final String path) {
+        final File dir = new File(outputDirectory + File.separator + TOOL_DIR + File.separator + path);
+
+        if (dir.exists()) {
+            if (!dir.isDirectory()) {
+                throw new IllegalArgumentException("Invalid path '" + path + "': it is not a directory");
+            }
+        } else {
+            dir.mkdirs();
+        }
+
+        return dir;
+    }
+
+    private File mkPkgDir(final String path) {
+        return mkdir(basePackage.replace('.', File.separatorChar) + File.separator + path);
+    }
+
+    private void writeFile(final String name, final File path, final VelocityContext ctx, final Template template,
+            final boolean append) throws MojoExecutionException {
+
+        if (!path.exists()) {
+            throw new IllegalArgumentException("Invalid base path '" + path.getAbsolutePath() + "'");
+        }
+
+        FileWriter writer = null;
+        try {
+            final File toBeWritten = new File(path, name);
+            if (!append && toBeWritten.exists()) {
+                throw new IllegalStateException("File '" + toBeWritten.getAbsolutePath() + "' already exists");
+            }
+            writer = new FileWriter(toBeWritten, append);
+            template.merge(ctx, writer);
+        } catch (IOException e) {
+            throw new MojoExecutionException("Error creating file '" + name + "'", e);
+        } finally {
+            IOUtils.closeQuietly(writer);
+        }
+    }
+
+    private VelocityContext newContext() {
+
+        final VelocityContext ctx = new VelocityContext();
+
+        ctx.put("utility", utility);
+        ctx.put("basePackage", basePackage);
+        ctx.put("schemaName", utility.getSchemaName());
+        ctx.put("namespace", utility.getNamespace());
+        ctx.put("namespaces", namespaces);
+
+        return ctx;
+    }
+
+    private void parseObj(final File base, final String pkg, final String name, final String out)
+            throws MojoExecutionException {
+
+        parseObj(base, false, pkg, name, out, Collections.<String, Object>emptyMap());
+    }
+
+    private void parseObj(
+            final File base,
+            final String pkg,
+            final String name,
+            final String out,
+            final Map<String, Object> objs)
+            throws MojoExecutionException {
+
+        parseObj(base, false, pkg, name, out, objs);
+    }
+
+    private void parseObj(
+            final File base,
+            final boolean append,
+            final String pkg,
+            final String name,
+            final String out,
+            final Map<String, Object> objs)
+            throws MojoExecutionException {
+
+        final VelocityContext ctx = newContext();
+        ctx.put("package", pkg);
+
+        if (objs != null) {
+            for (Map.Entry<String, Object> obj : objs.entrySet()) {
+                if (StringUtils.isNotBlank(obj.getKey()) && obj.getValue() != null) {
+                    ctx.put(obj.getKey(), obj.getValue());
+                }
+            }
+        }
+
+        final Template template = Velocity.getTemplate(name + ".vm");
+        writeFile(out, base, ctx, template, append);
+    }
+}
---
+++ plugin/src/main/java/com/msopentech/odatajclient/plugin/V4Utility.java
@@ -0,0 +1,289 @@
+/**
+ * Copyright © Microsoft Open Technologies, Inc.
+ *
+ * All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+ * ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A
+ * PARTICULAR PURPOSE, MERCHANTABILITY OR NON-INFRINGEMENT.
+ *
+ * See the Apache License, Version 2.0 for the specific language
+ * governing permissions and limitations under the License.
+ */
+package com.msopentech.odatajclient.plugin;
+
+import com.msopentech.odatajclient.engine.data.metadata.EdmType;
+import com.msopentech.odatajclient.engine.data.metadata.EdmV3Metadata;
+import com.msopentech.odatajclient.engine.data.metadata.EdmV3Type;
+import com.msopentech.odatajclient.engine.data.metadata.edm.AbstractEntitySet;
+import com.msopentech.odatajclient.engine.data.metadata.edm.AbstractEntityType;
+import com.msopentech.odatajclient.engine.data.metadata.edm.v3.Association;
+import com.msopentech.odatajclient.engine.data.metadata.edm.v3.AssociationEnd;
+import com.msopentech.odatajclient.engine.data.metadata.edm.AbstractProperty;
+import com.msopentech.odatajclient.engine.data.metadata.edm.PropertyRef;
+import com.msopentech.odatajclient.engine.data.metadata.edm.v3.EntityContainer;
+import com.msopentech.odatajclient.engine.data.metadata.edm.v3.EntityType;
+import com.msopentech.odatajclient.engine.data.metadata.edm.v3.FunctionImport;
+import com.msopentech.odatajclient.engine.data.metadata.edm.v3.Schema;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.apache.commons.lang.StringUtils;
+
+public class Utility {
+
+    private static final String FC_TARGET_PATH = "fcTargetPath";
+
+    private static final String FC_SOURCE_PATH = "fcSourcePath";
+
+    private static final String FC_KEEP_IN_CONTENT = "fcKeepInContent";
+
+    private static final String FC_CONTENT_KIND = "fcContentKind";
+
+    private static final String FC_NS_PREFIX = "fcNSPrefix";
+
+    private static final String FC_NS_URI = "fcNSURI";
+
+    private static final String TYPE_SUB_PKG = "types";
+
+    private final EdmV3Metadata metadata;
+
+    private final Schema schema;
+
+    private final String basePackage;
+
+    private final String schemaName;
+
+    private final String namespace;
+
+    private final Map<String, List<EntityType>> allEntityTypes = new HashMap<String, List<EntityType>>();
+
+    public Utility(final EdmV3Metadata metadata, final Schema schema, final String basePackage) {
+        this.metadata = metadata;
+        this.schema = schema;
+        this.basePackage = basePackage;
+        this.namespace = schema.getNamespace();
+        this.schemaName = schema.getAlias() == null ? getNameFromNS(namespace) : schema.getAlias();
+
+        for (Schema _schema : metadata.getSchemas()) {
+            allEntityTypes.put(_schema.getNamespace(), _schema.getEntityTypes());
+            if (StringUtils.isNotBlank(_schema.getAlias())) {
+                allEntityTypes.put(_schema.getAlias(), _schema.getEntityTypes());
+            }
+        }
+    }
+
+    public String getBasePackage() {
+        return basePackage;
+    }
+
+    public String getSchemaName() {
+        return schemaName;
+    }
+
+    public String getNamespace() {
+        return namespace;
+    }
+
+    public String capitalize(final String str) {
+        return StringUtils.capitalize(str);
+    }
+
+    public String uncapitalize(final String str) {
+        return StringUtils.uncapitalize(str);
+    }
+
+    public String getJavaType(final String typeExpression) {
+        final StringBuilder res = new StringBuilder();
+
+        final EdmType edmType = new EdmV3Type(metadata, typeExpression);
+
+        if (edmType.isCollection() && !edmType.isEntityType()) {
+            res.append("Collection<");
+        }
+
+        if ("Edm.Stream".equals(typeExpression)) {
+            res.append(InputStream.class.getName());
+        } else if (edmType.isSimpleType()) {
+            res.append(edmType.getSimpleType().javaType().getSimpleName());
+        } else if (edmType.isComplexType()) {
+            res.append(basePackage).append('.').append(edmType.getNamespaceOrAlias().toLowerCase()).append('.').
+                    append(TYPE_SUB_PKG).append('.').append(capitalize(edmType.getComplexType().getName()));
+        } else if (edmType.isEntityType()) {
+            res.append(basePackage).append('.').append(edmType.getNamespaceOrAlias().toLowerCase()).append('.').
+                    append(TYPE_SUB_PKG).append('.').append(capitalize(edmType.getEntityType().getName()));
+        } else if (edmType.isEnumType()) {
+            res.append(basePackage).append('.').append(edmType.getNamespaceOrAlias().toLowerCase()).
+                    append('.').append(TYPE_SUB_PKG).append('.').append(capitalize(edmType.getEnumType().getName()));
+        } else {
+            throw new IllegalArgumentException("Invalid type expression '" + typeExpression + "'");
+        }
+
+        if (edmType.isCollection()) {
+            if (edmType.isEntityType()) {
+                res.append("Collection");
+            } else {
+                res.append(">");
+            }
+        }
+
+        return res.toString();
+    }
+
+    public Map<String, String> getFcProperties(final AbstractProperty property) {
+        final Map<String, String> fcProps = new HashMap<String, String>();
+
+        if (StringUtils.isNotBlank(property.getFcTargetPath())) {
+            fcProps.put(FC_TARGET_PATH, property.getFcTargetPath());
+        }
+        if (StringUtils.isNotBlank(property.getFcSourcePath())) {
+            fcProps.put(FC_SOURCE_PATH, property.getFcSourcePath());
+        }
+        if (StringUtils.isNotBlank(property.getFcNSPrefix())) {
+            fcProps.put(FC_NS_PREFIX, property.getFcNSPrefix());
+        }
+        if (StringUtils.isNotBlank(property.getFcNSURI())) {
+            fcProps.put(FC_NS_URI, property.getFcNSURI());
+        }
+        fcProps.put(FC_CONTENT_KIND, property.getFcContentKind().name());
+        fcProps.put(FC_KEEP_IN_CONTENT, Boolean.toString(property.isFcKeepInContent()));
+
+        return fcProps;
+    }
+
+    public EdmType getEdmType(final AbstractEntitySet entitySet) {
+        return new EdmV3Type(metadata, entitySet.getEntityType());
+    }
+
+    public Map<String, String> getEntityKeyType(final AbstractEntitySet entitySet) {
+        return getEntityKeyType(getEdmType(entitySet).getEntityType());
+    }
+
+    public Map<String, String> getEntityKeyType(final AbstractEntityType entityType) {
+        AbstractEntityType baseType = entityType;
+        while (baseType.getKey() == null && baseType.getBaseType() != null) {
+            baseType = new EdmV3Type(metadata, baseType.getBaseType()).getEntityType();
+        }
+
+        final List<String> properties = new ArrayList<String>();
+        for (PropertyRef pref : baseType.getKey().getPropertyRefs()) {
+            properties.add(pref.getName());
+        }
+
+        final Map<String, String> res = new HashMap<String, String>();
+
+        for (AbstractProperty prop : baseType.getProperties()) {
+            if (properties.contains(prop.getName())) {
+                res.put(prop.getName(), getJavaType(prop.getType()));
+            }
+        }
+
+        return res;
+    }
+
+    public String getNavigationRoleType(final String associationName, final String associationRole) {
+        final String name = getNameFromNS(associationName);
+        final Association association = schema.getAssociation(name);
+        if (association != null) {
+            for (AssociationEnd end : association.getEnds()) {
+                if (end.getRole().equalsIgnoreCase(associationRole)) {
+                    return "*".equals(end.getMultiplicity())
+                            ? "Collection(" + end.getType() + ")"
+                            : end.getType();
+                }
+            }
+        }
+
+        return associationRole;
+    }
+
+    public final String getNameInNamespace(final String name) {
+        return schema.getNamespace() + "." + name;
+    }
+
+    public final String getNameInNamespace(final EdmType entityType) {
+        return entityType.getNamespaceOrAlias() + "." + entityType.getEntityType().getName();
+    }
+
+    public final String getNameFromNS(final String ns) {
+        return getNameFromNS(ns, false);
+    }
+
+    public final String getNameFromNS(final String ns, final boolean toLowerCase) {
+        final int lastpt = ns.lastIndexOf('.');
+        final String res = ns.substring(lastpt < 0 ? 0 : lastpt + 1);
+        return toLowerCase ? res.toLowerCase() : res;
+    }
+
+    public boolean isSameType(
+            final String entityTypeExpression, final String fullTypeExpression, final boolean collection) {
+
+        final Set<String> types = new HashSet<String>(2);
+
+        types.add((collection ? "Collection(" : StringUtils.EMPTY)
+                + getNameInNamespace(entityTypeExpression)
+                + (collection ? ")" : StringUtils.EMPTY));
+        if (StringUtils.isNotBlank(schema.getAlias())) {
+            types.add((collection ? "Collection(" : StringUtils.EMPTY)
+                    + schema.getAlias() + "." + entityTypeExpression
+                    + (collection ? ")" : StringUtils.EMPTY));
+        }
+
+        return types.contains(fullTypeExpression);
+    }
+
+    private void populateDescendants(final EdmType base, final List<String> descendants) {
+        for (Map.Entry<String, List<EntityType>> entry : allEntityTypes.entrySet()) {
+            for (EntityType type : entry.getValue()) {
+                if (StringUtils.isNotBlank(type.getBaseType())
+                        && base.getEntityType().getName().equals(getNameFromNS(type.getBaseType()))) {
+
+                    final EdmType entityType = new EdmV3Type(metadata, entry.getKey() + "." + type.getName());
+
+                    descendants.add(getNameInNamespace(entityType));
+                    populateDescendants(entityType, descendants);
+                }
+            }
+        }
+    }
+
+    public List<String> getDescendantsOrSelf(final EdmType entityType) {
+        final List<String> descendants = new ArrayList<String>();
+
+        descendants.add(getNameInNamespace(entityType));
+        populateDescendants(entityType, descendants);
+
+        return descendants;
+    }
+
+    public List<FunctionImport> getFunctionImportsBoundTo(
+            final String typeExpression, final boolean collection) {
+
+        final List<FunctionImport> result = new ArrayList<FunctionImport>();
+
+        for (EntityContainer entityContainer : schema.getEntityContainers()) {
+            for (FunctionImport functionImport : entityContainer.getFunctionImports()) {
+                if (functionImport.isBindable()) {
+                    for (int i = 0; i < functionImport.getParameters().size(); i++) {
+                        if (isSameType(typeExpression, functionImport.getParameters().get(i).getType(), collection)) {
+                            result.add(functionImport);
+                        }
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
